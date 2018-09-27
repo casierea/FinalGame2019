a7ee9467cad63089a2ec4578a3c41535c4cd1a44
Casandra Roell <dancingmongoose@gmail.com>
2018-09-27 13:57:55 -0600

    Add Health, Total Money
    Redo Sept.19.
    Add Health event. CLick on cube and health goes down. Click on cube money goes up

---
+++ test_project/Assets/AddValue.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+
+public class AddValue : ScriptableObject
+{
+	public FloatData ValueObj;
+	public FloatData MaxValue;
+	public FloatData MinValue;
+	
+//something else that can do. not needed for script
+	public UnityEvent EventMax;
+	public UnityEvent EventMin;
+
+	public void AddValueToObject(FloatData data)
+	{
+		ValueObj.Value += data.Value;
+
+		//more for fun
+		if (ValueObj.Value >= MaxValue.Value)
+		{
+			EventMax.Invoke();
+		}
+		if (ValueObj.Value <= MinValue.Value)
+		{
+			EventMin.Invoke();
+
+		}
+		}
+
+		
+	}
