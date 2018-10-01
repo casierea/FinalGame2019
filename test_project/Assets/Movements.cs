243a33ce386dd392a2cb314a34f9b20012c2ccde
Casandra Roell <dancingmongoose@gmail.com>
2018-09-30 21:25:18 -0600

    practice

---
+++ test_project/Assets/Movements.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEditor;
+using UnityEngine;
+
+//public class Movements : MonoBehaviour {
+//
+//	public enum MovementType
+//	{
+//		MoveX,
+//		MoveY,
+//		MoveZ
+//	}
+//
+//	public MovementType CurrentMovementType;
+//
+//	void Start()
+//	{
+//		void Update()
+//		{
+//			switch (CurrentMovementType)
+//			{
+//					case MovementType.MoveX
+//						break;
+//			}
+//		}
+//	}
