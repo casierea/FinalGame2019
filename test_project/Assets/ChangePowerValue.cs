be906e7e7dda8c5645e12a6da67710b86caaa22d
Casandra Roell <dancingmongoose@gmail.com>
2018-10-01 17:51:26 -0600

    Homework
    Attempting to create one script for one asset to do many things

---
+++ test_project/Assets/ChangePowerValue.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ChangePowerValue : ScriptableObject {
+
+
+//Add Value
+    public FloatData Data;
+    
+    public void OnPowerUp(FloatData outsideData)
+    {
+        outsideData.Value += Data.Value;
+    }
+//Subtract Value
+    public void OnPowerDown(FloatData outsideData)
+    {
+        outsideData.Value -= Data.Value;
+    }
+}
+
