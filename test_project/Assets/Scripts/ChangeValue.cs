7c027acffe2165bd1e106854a9d3280af587ab4e
Casandra Roell <dancingmongoose@gmail.com>
2018-10-01 21:45:12 -0600

    HW Divide-Sub_add_Multiply
    Hw in one asset.
    Zoya and events.

---
+++ test_project/Assets/Scripts/ChangeValue.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+
+public class ChangeValue : ScriptableObject
+{
+	public FloatData ValueObj;
+	public FloatData MaxValue;
+	public FloatData MinValue;
+	
+//something else that can do. not needed for script
+	public UnityEvent EventMax;
+	public UnityEvent EventMin;
+
+	public void AddValueToObject(FloatData data)
+	{
+		ValueObj.Value += data.Value;
+
+		//more for fun
+		//if (ValueObj.Value >= MaxValue.Value)
+		//{
+			//EventMax.Invoke();
+		//}
+		//if (ValueObj.Value <= MinValue.Value)
+		//{
+			//EventMin.Invoke();
+
+		}
+	
+	public void SubtractValueFromObject(FloatData data)
+	{
+		ValueObj.Value -= data.Value;
+		//data.Value -= ValueObj.Value;
+		
+	}
+	
+	public void MultiplyValueBy(FloatData data)
+	{
+		ValueObj.Value *= data.Value;
+		//data.Value *= ObjValue.Value;
+
+	}
+
+	public void DivideValueBy(FloatData data)
+	{
+		if (ValueObj.Value != 0.0f)
+		{
+			ValueObj.Value /= data.Value;
+		}
+	}
+
+		
+}
---
+++ test_project/Assets/Scripts/MinMaxChange.cs
@@ -0,0 +1,55 @@
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Runtime.InteropServices;
+//using UnityEngine;
+//using UnityEngine.Events;
+//
+//[CreateAssetMenu]
+//
+//public class MinMaxChangeValue : ScriptableObject
+//{
+//	public FloatData ChangeValue;
+//	public FloatData MaxValue;
+//	public FloatData MinValue;
+//	public object gameObject
+////something else that can do. not needed for script
+//	public UnityEvent EventMax;
+//	public UnityEvent EventMin;
+//	
+//
+//	public void AddValueToObject(FloatData data)
+//	{
+//		data.Value += ChangeValue.Value;
+//		//more for fun
+//		if (ChangeValue.Value >= MaxValue.Value)
+//		{
+//			EventMax.Invoke();
+//		}
+//
+//		if (ChangeValue.Value <= MinValue.Value)
+//		{
+//			EventMin.Invoke();
+//
+//		}
+//	}
+//
+//	public void SubtractValueFromObject(FloatData data)
+//		{
+//			data.Value -= ChangeValue.Value;
+//		
+//		}
+//	
+//		public void MultiplyValueBy(FloatData data)
+//		{
+//			data.Value *= ChangeValue.Value;
+//		}
+//
+//		public void DivideValueBy(FloatData data)
+//		{
+//			if (ChangeValue.Value != 0.0f)
+//			{
+//				data.Value /= ChangeValue.Value;
+//			}
+//		}
+//	}
