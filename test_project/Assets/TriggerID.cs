d8aa3f6f2f3f50499c454514acd958f419a96c39
Casandra Roell <dancingmongoose@gmail.com>
2018-10-02 21:42:11 -0600

    IfStatements
    key and lock. lights on or off

---
+++ test_project/Assets/TriggerID.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using System.Security.Permissions;
+//using UnityEngine;
+//using UnityEngine.Events;
+//
+//public class TriggerId : MonoBehaviour
+//{
+//	public NameID ID;
+//	public UnityEvent OnMatch;
+//
+//	private void OnTriggerEnter(Collider obj)
+//	{
+//		if (ID == obj.GetComponent<ObjectID>().ID)
+//		{
+//				OnMatch.Invoke();
+//				print("Match");
+//			}
+//		}
+//	}
+//	
+
