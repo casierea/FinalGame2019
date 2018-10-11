9e835de83ca35ca684b1590d32384dda5bbf9661
Casandra Roell <dancingmongoose@gmail.com>
2018-10-10 21:26:20 -0600

    redo of coin and power ups

---
+++ test_project/Assets/Speak.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Speak : MonoBehaviour
+{
+
+	public int HappyLevel = 10;
+	public int Money = 100;
+	public int Beers = 1;
+		
+	// Use this for initialization
+	void Awake()
+	{
+		print("I love cheese");
+	}
+	}
+
