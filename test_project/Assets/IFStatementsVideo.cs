58938161d70af6d2548162a975a7562ad97b3790
Casandra Roell <dancingmongoose@gmail.com>
2018-10-13 21:17:35 -0600

    practice video

---
+++ test_project/Assets/IFStatementsVideo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IFStatementsVideo : MonoBehaviour
+{
+	public int A;
+	public int B;
+	public int C; 
+	
+	public string Password;
+	public bool OnSwitch;
+	void Update () {
+		if (OnSwitch)
+		{
+			print("The Light is on");
+		}
+
+		if (Password == "Pikachu") ;
+		{
+			print("Correct!");
+		}
+
+		if (A + B == C)
+		{
+			print("A plus B equals C");
+
+		}
+	}
+}
