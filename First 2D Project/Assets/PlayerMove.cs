8b4f459abef665ba76dd39ec79d9b374ba136bf1
Casandra Roell <dancingmongoose@gmail.com>
2018-10-14 19:38:26 -0600

    First 2D project

---
+++ First 2D Project/Assets/PlayerMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+    
+    public int PlayerSpeed = 10;
+    public bool FacingRight =  true;
+    public int PlayerJumpPower = 1250;
+	public float MoveX;
+	
+
+
+	void Update () {
+		PlayerMover();
+}
+
+void PlayerMover(){
+//Controls
+	MoveX = Input.GetAxis("Horizontal");
+//Animatioans
+//Player Direction
+if (MoveX < 0.0f && FacingRight == false)
+{
+	FlipPlayer();
+} else if (MoveX > 0.0f && FacingRight == true){
+FlipPlayer();
+}
+//Physics
+	gameObject.GetComponent<Rigidbody2D>().velocity =
+		new Vector2(MoveX * PlayerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+}
+void Jump(){
+//Jump code
+}
+void FlipPlayer(){
+
+}
+}
