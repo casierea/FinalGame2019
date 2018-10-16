37a02a71ffd242705b35bc7a2c984dc90f2ed866
Casandra Roell <dancingmongoose@gmail.com>
2018-10-15 21:42:17 -0600

    OrganizeFoldersAndCollections
    organize folders and collections

---
+++ test_project/Assets/Scripts/ClassScripts/Instancing.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Instancing : MonoBehaviour
+{
+
+	public GameObject Instance;
+	
+	// Use this for initialization
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown(0))
+		//if (Input.GetKeyDown(KeyCode.X)) // could do GetButtonDown and use mouse button
+		{
+			Instantiate(Instance, transform.position, transform.rotation);
+			
+									//yield return new WaitForSeconds(1);
+		} 
+									// quat= rotate values
+	}
+}
