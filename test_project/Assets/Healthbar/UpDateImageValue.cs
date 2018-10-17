b2f96426f30605a0d02f768a9fa393d8b608928d
Unknown <dancingmongoose@gmail.com>
2018-10-17 15:42:30 -0600

    HealthBar

---
+++ test_project/Assets/Healthbar/UpDateImageValue.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//using UnityEngine.Experimental.UIElements;
+
+
+public class UpDateImageValue : MonoBehaviour
+{
+
+
+	public FloatData Data;
+
+	private Image image;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		image = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		image.fillAmount = Data.Value;
+	}
+}
