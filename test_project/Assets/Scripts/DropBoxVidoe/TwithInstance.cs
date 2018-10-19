69c9fdd61bced405d629574df509327cf0010204
Casandra Roell <dancingmongoose@gmail.com>
2018-10-18 19:07:51 -0600

    Instance with bullet

---
+++ test_project/Assets/Scripts/DropBoxVidoe/TwithInstance.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class TwithInstance : MonoBehaviour
+{
+
+	public GameObject Instance;
+
+	 private void Start ()
+	 {
+		 Instantiate(Instance);
+	 }
+	
+
+}
