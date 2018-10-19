7bf7a9e84a23731347def634d9996b2b8c06914d
Casandra Roell <dancingmongoose@gmail.com>
2018-10-18 20:00:27 -0600

    InstanceBase

---
+++ test_project/Assets/Scripts/DropBoxVidoe/InstancingBase.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class InstancingBase : ScriptableObject
+{
+    private Vector3 position;
+    
+    public GameObject Prefab;
+
+    public void InstanceObject(GameObject prefab)
+    {
+        position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        position.z = 0;
+        Instantiate(prefab, position, Quaternion.identity);
+    }
+}
