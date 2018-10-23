7fa9de39197de6d3a41c2622b54cddc74ddc4899
Casandra Roell <dancingmongoose@gmail.com>
2018-10-23 16:51:53 -0600

    Math Operators

---
+++ test_project/Assets/Scripts/MathOperators.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MathOperators : MonoBehaviour
+{
+
+	public int A;
+
+	public int B;
+
+	public int C;
+	// Use this for initialization
+	void Start ()
+	{
+		//C = A +B;
+		//C++;
+		C += A;
+		C --;
+		C = A - B;
+		C -= A;
+		C = A * B;
+		C *= A;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
