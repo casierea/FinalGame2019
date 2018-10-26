1c7b0df977543f18ed2b0bc755fddc5b46f38987
Casandra Roell <dancingmongoose@gmail.com>
2018-10-25 20:53:17 -0600

    LoopsPractice

--- test_project/Assets/Loops.cs
+++ test_project/Assets/Loops.cs
@@ -8,7 +8,9 @@ public class Loops : MonoBehaviour
 
 	public string[] Names;
 	public GameObject [] Things;
-	
+	public GameObject[] Shapes;
+	public string[] Ducks;
+	public GameObject [] Anphibians;
 	
 	// Use this for initialization
 	void Start ()
@@ -21,6 +23,21 @@ public class Loops : MonoBehaviour
 		foreach (var thing in Things)
 		{
 			print(thing);
+		}
+
+		foreach (var shape in Shapes)
+		{
+			print(shape);	
+		}
+
+		foreach (var duckie in Ducks)
+		{
+			print(duckie);
+		}
+
+		foreach (var frog in Anphibians)
+		{
+			print(frog);
 		}
 	}
 	
