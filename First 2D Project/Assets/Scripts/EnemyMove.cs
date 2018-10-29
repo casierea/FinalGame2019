e1229e12b2d261fb8c9b928c15c300fda66edebc
Casandra Roell <dancingmongoose@gmail.com>
2018-10-28 19:29:54 -0600

    EnermyMove

--- First 2D Project/Assets/Scripts/EnemyMove.cs
+++ First 2D Project/Assets/Scripts/EnemyMove.cs
@@ -4,16 +4,18 @@
 public class EnemyMove : MonoBehaviour {
     public int EnemySpeed;
     public int XMoveDirection;
-    
+
+    public float DistanceToSide = 1.0f;
     // Update is called once per frame
     void Update ()
     {
         RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2 (XMoveDirection, 0));
-        gameObject.GetComponent<Rigidbody>().velocity = new Vector2(XMoveDirection,0) * EnemySpeed;
-
-        if (hit.distance < .7f)
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(XMoveDirection,0) * EnemySpeed;
+Debug.Log(hit.distance);
+        if (hit.collider != null && hit.distance < DistanceToSide && hit.collider.tag != "Enemy")
         {
             Flip();
+            Debug.Log("Collider hit" + hit.collider.tag.ToString());
             if (hit.collider.tag == "Player")
             {
                 Destroy(hit.collider.gameObject);
