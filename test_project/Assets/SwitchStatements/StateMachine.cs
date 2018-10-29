a29059c723286610c689d5d20f7422e8cc2a8ad8
Unknown <dancingmongoose@gmail.com>
2018-10-29 15:18:54 -0600

    classs

---
+++ test_project/Assets/SwitchStatements/StateMachine.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class StateMachine : ScriptableObject{
+public GameStates.States GameState;
+public UnityEvent Starting, Loading, Playing, Ending, Dying;
+	
+//StateMachine.Can do a unityevent.
+	
+// Use this for initialization
+void Start () {
+		
+}
+	
+// Update is called once per frame
+public void Run () {
+switch (GameState)
+{
+case GameStates.States.Starting:
+//print("We are starting the Game.");
+break;
+				
+case GameStates.States.Loading:
+//print("Loading!");
+break;
+				
+case GameStates.States.Playing:
+//print("We are playing the Game.");
+break;
+				
+case GameStates.States.Ending:
+//print("We are ending the Game.");
+break;
+					
+}
+}
+}
+
+	
---
+++ test_project/Assets/SwitchStatements/SwitchStatements.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SwitchStatements : MonoBehaviour
+{
+
+	public GameStates.States GameState;
+	public UnityEvent Starting, Loading, Playing, Ending, Dying;
+	
+	//StateMachine.Can do a unityevent.
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (GameState)
+		{
+				case GameStates.States.Starting:
+				print("We are starting the Game.");
+					break;
+				
+				case GameStates.States.Loading:
+					print("Loading!");
+					break;
+				
+				case GameStates.States.Playing:
+				print("We are playing the Game.");
+					break;
+				
+				case GameStates.States.Ending:
+					print("We are ending the Game.");
+					break;
+					
+		}
+	}
+}
