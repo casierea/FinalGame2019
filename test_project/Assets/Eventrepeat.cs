16acc2277467d448f7631698a61a97b608ccecbf
Unknown <dancingmongoose@gmail.com>
2018-11-07 15:28:52 -0700

    class

---
+++ test_project/Assets/Eventrepeat.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Eventrepeat : MonoBehaviour
+{
+	public UnityEvent[] OnReapeat;
+	
+	public float Seconds = 2;
+	private int i = 0;
+	
+		IEnumerator Start(){
+			while (true)
+			{
+				yield return new WaitForSeconds(Seconds);
+				
+				OnReapeat[i].Invoke();
+				
+				if (i < OnReapeat.Length-1)
+				{
+					i++;
+				}
+				else
+				{
+					i = 0;
+				}
+				
+			}
+
+	}
+}
+	
