c04d5d5ea5dc6bef4cc7cc1e716f82c26a71a8d7
Casandra Roell <dancingmongoose@gmail.com>
2018-11-13 21:53:31 -0700

    animation

---
+++ test_project/Assets/Scripts/Ai/BasicAi.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEditor.Experimental.UIElements.GraphView;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BasicAi : MonoBehaviour
+{
+	private NavMeshAgent agent;
+
+	//public Transform Destination;
+	public Transform Target;
+	//public Transform Destination3;
+
+
+	void Start()
+	{
+		agent = GetComponent<NavMeshAgent>();
+	}
+
+
+	void Update()
+	{
+		//if (Destination != null) agent.destination = Destination.position;
+		if (Target != null) agent.destination = Target.position;
+	}
+
+}
