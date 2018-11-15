76b33429c416bf61110c1946aa9d0baa9f5d3a97
Casandra Roell <dancingmongoose@gmail.com>
2018-11-14 19:53:36 -0700

    tutorial

---
+++ test_project/Assets/ObjectControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectControl : MonoBehaviour
+{
+	public bool Item; // if true item can be used
+
+	public bool Speak; // true = character can talk to player
+
+	public string CommunicateData; // text to communicate to player
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
