7be3a689dd5446d60adce2788127bd6f3d4e6c49
Casandra Roell <dancingmongoose@gmail.com>
2018-11-14 18:55:18 -0700

    Top 100 players

---
+++ test_project/Assets/TopScores.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopScores : MonoBehaviour
+{
+	public string[] PlayerNames;
+	
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < PlayerNames.Length; i++)
+		{
+			print (PlayerNames [i]);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
