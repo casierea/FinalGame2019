7950ee01a0d6c576e8cbcb425343a2363662f94e
Casandra Roell <dancingmongoose@gmail.com>
2018-11-17 21:43:06 -0700

    nothing works player move

---
+++ test_project/Assets/NewPlayerMove.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewPlayerMove : MonoBehaviour
+{
+
+    public float smooth = 1f;
+    public float speed = 10.0F;
+    public float jumpSpeed = 15.0F;
+    public float gravity = 20.0F;
+    private Quaternion lookLeft;
+    private Quaternion lookRight;
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Start()
+    {
+        Cursor.visible = false;
+        Time.timeScale = 1;
+
+        lookRight = transform.rotation;
+        lookLeft = lookRight * Quaternion.Euler(0, 180, 0);
+    }
+
+    void Update()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+        moveDirection = new Vector3((Input.GetAxis("Horizontal")), 0, 0);
+        
+        if (Input.GetKey(KeyCode.A) | Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.rotation = lookLeft;
+            moveDirection = transform.TransformDirection(-moveDirection);
+            moveDirection *= speed;
+        }
+
+        if (Input.GetKey(KeyCode.D) | Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.rotation = lookRight;
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+        }
+      
+        //Only jump if controller is grounded
+        if (controller.isGrounded & Input.GetButton("Jump"))
+        {
+            jump();
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime); //move character
+    }
+
+    void jump()
+    {
+        moveDirection.y = jumpSpeed;
+    }
+
+}
--- test_project/Assets/Scripts/Character/PlayerMove.cs
+++ test_project/Assets/Scripts/Character/PlayerMove.cs
@@ -1,56 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class PlayerMove : MonoBehaviour {
+    
+    public float smooth = 1f;
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Quaternion lookLeft;
+    private Quaternion lookRight;
+    private Vector3 moveDirection = Vector3.zero;
 
-		public float smooth= 1f;
-		public float speed = 10.0F;
-		public float jumpSpeed = 8.0F;
-		public float gravity = 20.0F;
-		private Quaternion lookLeft;
-		private Quaternion lookRight;
-		private Vector3 moveDirection = Vector3.zero;
+    void Start(){
+        Cursor.visible = false;
+       
+        Time.timeScale = 1;
 
-		void Start(){
+        lookRight = transform.rotation;
+        lookLeft = lookRight * Quaternion.Euler(0, 180, 0); 
+    }
+
+    void Update() {
+        CharacterController controller = GetComponent<CharacterController>();
+        if (controller.isGrounded) {
+            moveDirection = new Vector3(-(Input.GetAxis("Horizontal")), 0, 0);
+
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+
+            if (Input.GetKey(KeyCode.LeftArrow)){
+
+                transform.rotation = lookLeft;
+                moveDirection = transform.TransformDirection(-moveDirection);
+                moveDirection *= speed;
+            }
+
+            if (Input.GetKey(KeyCode.RightArrow)){
+                transform.rotation = lookRight;
+                moveDirection = transform.TransformDirection(moveDirection);
+                moveDirection *= speed;
+            }
 			
-			//Can I use transform.rotation?
-			
-			Cursor.visible = false;
-			Time.timeScale = 1;
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
 
-			lookRight = transform.rotation;
-			lookLeft = lookRight * Quaternion.Euler(0, 180, 0); 
-		}
-
-		void Update() 
-		{
-			//controller = GetComponent<CharacterController>();
-			CharacterController controller = GetComponent<CharacterController>();
-			if (controller.isGrounded) {
-
-				moveDirection = new Vector3((Input.GetAxis("Horizontal")), 0, 0);
-
-				if (Input.GetButton("Jump"))
-				{
-					moveDirection.y = jumpSpeed;
-				}
-
-				if (Input.GetKey(KeyCode.A)){
-
-					transform.rotation = lookLeft;
-					moveDirection = transform.TransformDirection(-moveDirection);
-					moveDirection *= speed;
-				}
-
-				if (Input.GetKey(KeyCode.D)){
-					transform.rotation = lookRight;
-					moveDirection = transform.TransformDirection(moveDirection);
-					moveDirection *= speed;
-				}
-			
-			}
-			moveDirection.y -= gravity * Time.deltaTime;
-			controller.Move(moveDirection * Time.deltaTime);
-		}
-	}
