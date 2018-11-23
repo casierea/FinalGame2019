d8bd6c6af1e31e7c040bf3d3ba23add1ed418250
Casandra Roell <dancingmongoose@gmail.com>
2018-11-22 21:07:40 -0700

    PickUp Cubes and Rotation

---
+++ test_project/Assets/BallGame/Rotator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+	void Update (){ //Rotates object
+		
+		transform.Rotate(new Vector3(15,30,45) * Time.deltaTime);
+	}
+}
