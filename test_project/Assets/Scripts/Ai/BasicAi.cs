aa9bbb9c9fcffdf315b5c5b61ff5f5663b05d097
Casie <dancingmongoose@gmail.com>
2018-11-26 19:32:04 -0700

    Ai Movemnet
    not moving right. need help with script and destroy on collision

--- test_project/Assets/Scripts/Ai/BasicAi.cs
+++ test_project/Assets/Scripts/Ai/BasicAi.cs
@@ -6,15 +6,21 @@ using UnityEngine.AI;
 public class BasicAi : MonoBehaviour
 {
 	private NavMeshAgent agent;
-
+//new information from player controller script
+	private Quaternion lookLeft;
+	private Quaternion lookRight;
+	
 	//public Transform Destination;
 	public Transform Target;
 	public Transform Target02;
-
+	
 
 	void Start()
 	{
 		agent = GetComponent<NavMeshAgent>();
+
+		lookRight = transform.rotation;
+		lookLeft = lookRight * Quaternion.Euler(0, 180, 0);
 	}
 
 
@@ -22,6 +28,20 @@ public class BasicAi : MonoBehaviour
 	{
 		//if (Destination != null) agent.destination = Destination.position;
 		if (Target != null) agent.destination = Target.position;
-	}
+		
+		if (Target.position.x <= gameObject.transform.position.x)
+		{
+			transform.rotation = lookLeft;
+		}
+		else if (Target.position.x < gameObject.transform.position.x)
+		{
+			transform.rotation = lookRight;
+			
+		}
+		var currentPosition = gameObject.transform.position;
+		gameObject.transform.position = new Vector3(currentPosition.x,currentPosition.y,0f);
+	}	
 
 }
+	
+
