4d2a1c5a124400adc933b80280786925e971541f
Casie <dancingmongoose@gmail.com>
2018-11-26 20:45:22 -0700

    rotate script makes object spin upside down

--- test_project/Assets/AnimateRote.cs
+++ test_project/Assets/AnimateRote.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnimaRotae : MonoBehaviour {
+public class AnimateRote : MonoBehaviour {
 
+	
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+		var currentRotation = gameObject.transform.rotation;
+		var newZ = currentRotation.z = -10;
+		gameObject.transform.rotation = new Quaternion(currentRotation.x, currentRotation.y, newZ, currentRotation.w);
 	}
 }
