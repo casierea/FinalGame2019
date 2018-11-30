20fac9ad50ed3ea9c86765a2567ef47e6f6dfc07
Casie <dancingmongoose@gmail.com>
2018-11-29 21:28:22 -0700

    Prefab fun and enemy shoot script

---
+++ test_project/Assets/Scripts/SemesterFinal/EnemyFire.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine.Editor;
+using UnityEngine;
+
+public class EnemyFire : MonoBehaviour
+
+{
+	public GameObject EnemyShoot;
+
+	IEnumerator Start()
+	{
+		while (true)
+		{
+			Instantiate(EnemyShoot, transform.position, Quaternion.identity);
+			yield return new WaitForSeconds(1);
+		}
+
+	}
+
+}
