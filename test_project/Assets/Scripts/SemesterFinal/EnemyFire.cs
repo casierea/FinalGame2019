aa32352edb3ac7062144e311fe2b017b78ccab1c
Casie <dancingmongoose@gmail.com>
2018-12-02 21:00:38 -0700

    Robot Shooter
    needs trigger to work to trigger bullet at player

--- test_project/Assets/Scripts/SemesterFinal/EnemyFire.cs
+++ test_project/Assets/Scripts/SemesterFinal/EnemyFire.cs
@@ -7,14 +7,41 @@ public class EnemyFire : MonoBehaviour
 
 {
 	public GameObject EnemyShoot;
+	public FloatData BulletSpeed;
+	private float ShootyForce;
+	private bool FacingRight;
+	private void Update()
+	{
+		FacingRight = gameObject.GetComponentInParent<SpriteRenderer>().flipX;
+	}
 
-	IEnumerator Start()
+	private void OnTriggerEnter(Collider other)
+	{	Debug.Log("Player tag");
+		
+		if (other.tag == "Player")
+		{
+			StartCoroutine("BeginShoot");
+		}
+	}
+
+	IEnumerator BeginShoot()
 	{
 		while (true)
-		{
-			Instantiate(EnemyShoot, transform.position, Quaternion.identity);
+		{	Debug.Log("in Whileloop");
+			if (FacingRight == false)
+			{
+				ShootyForce = -BulletSpeed.Value;
+			}
+			else
+			{
+				ShootyForce = BulletSpeed.Value;
+			}
+			GameObject Clone;
+			Clone = Instantiate(EnemyShoot, transform.position, Quaternion.identity);
+			Clone.GetComponent<Rigidbody>().AddForce(ShootyForce,0,0);
 			yield return new WaitForSeconds(1);
 		}
+		
 
 	}
 
