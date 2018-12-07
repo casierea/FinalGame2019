0b77f08a516988604b56c23942cb885c59491afc
Casandra Roell <dancingmongoose@gmail.com>
2018-12-07 12:48:30 -0700

    Bomb gives damage

---
+++ test_project/Assets/Scripts/HealOnCollision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealOnCollision : MonoBehaviour {
+
+    public FloatData Heal;
+	
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<RedoObjHealth>())
+        {
+            other.GetComponent<RedoObjHealth>().DamageHealth(Heal.Value);
+        }
+
+        if (other.gameObject.GetComponent<MultiObjHealth>())
+        {
+            other.GetComponent<MultiObjHealth>().DamageHealth(Heal.Value);
+        }
+    }
+}
+
