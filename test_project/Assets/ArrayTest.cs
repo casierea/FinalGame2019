11bd71dd2d955f5dd5c0db08b7b2558a63c2192a
Casandra Roell <dancingmongoose@gmail.com>
2018-12-09 21:01:03 -0700

    extra array

---
+++ test_project/Assets/ArrayTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrayTest : MonoBehaviour
+{
+    public string[] BadGuys;
+
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < BadGuys.Length; i++)
+        {
+            print(BadGuys[i]);
+        }
+    }
+}
