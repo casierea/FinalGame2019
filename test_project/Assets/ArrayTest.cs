f4f86060471eeb1f04a9d26cd924a27a1ef2d606
Casandra Roell <dancingmongoose@gmail.com>
2018-12-10 08:51:35 -0700

    arraytest script

--- test_project/Assets/ArrayTest.cs
+++ test_project/Assets/ArrayTest.cs
@@ -1,23 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class ArrayTest : MonoBehaviour
 {
     public string[] BadGuys;
-    public int [] Numbers; 
-
+    public int[] Numbers;
+    public int[] ThreeNums = {5,6,9}; 
+    
     // Use this for initialization
     void Start()
     {
+        for (int i = 0; i < ThreeNums.Length; i++)
+        {
+            Debug.Log("help"+i);
+            Debug.Log(ThreeNums[i]);
+        }
         for (int i = 0; i < BadGuys.Length; i++)
         {
             print(BadGuys[i]);
+            Debug.Log(BadGuys[i]);
         }
         foreach (var number in Numbers)
         {
             print(number);	
         }
     }
+
+   
 }
